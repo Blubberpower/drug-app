e2858801f8a87f4f120d08e892a8d6510f66f319
Blubberpower <48146654+Blubberpower@users.noreply.github.com>
2020-10-14 07:16:29 -0400

    Initial commit

---
+++ DRUGAPP.js
@@ -0,0 +1,52 @@
+// hide credit card info after payment example
+//var ass = document.querySelectorAll(".page")[0,1];
+//ass.textContent ="lol";
+
+
+//char * drugs  <--- i dunno wat this is for lol
+
+var isPaid = false;
+
+  
+
+function searchFunction() {
+  if (isPaid == false) {
+    document.getElementById("drugsearch").disabled = true;
+  }
+
+  else{
+    document.getElementById("drugsearch").disabled = false;
+	}
+  }
+
+
+
+  function checkinfo() {
+
+    alert("INFO GOOD");
+    isPaid = true;
+    searchFunction();
+    var ass = document.querySelectorAll(".page")[0];
+    var ass2 = document.querySelectorAll(".cod")[0];
+    ass.textContent = "";
+    ass2.innerHTML ="<h1>YOUR BILL IS 14 DOLLARS A YEAR. THANKYOU.</h1>";
+
+    }
+  
+  
+
+
+    function drugappend() {
+      document.getElementById("drugForm").onsubmit = function() {
+      var msgElement = document.getElementById("q");
+      msgElement.value = msgElement.value + ' drug';
+      return true;
+      };
+    };
+  
+  
+
+
+
+
+
